55c0298092c4a8398a14c377fe3715469c28b8af
vararais <varadigmaisnann@gmail.com>
2025-11-09 23:24:30 +0700

    Membuat UI untuk gambar welcome page

--- app/src/main/java/com/example/questnavigastugas_049/view/PageWelcome.kt
+++ app/src/main/java/com/example/questnavigastugas_049/view/PageWelcome.kt
@@ -1,2 +1,58 @@
 package com.example.questnavigastugas_049.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigastugas_049.R
+
+@Composable
+fun WelcomeScreen(
+    onMasukClick: () -> Unit
+) {
+    var isLoading by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.selamat_datang),
+            fontSize = 24.sp
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.welcomee),
+            contentDescription = "Logo Aplikasi",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = stringResource(id = R.string.nama_anan))
+        Text(text = stringResource(id = R.string.nim_anan))
+
+        Spacer(modifier = Modifier.height(48.dp))
