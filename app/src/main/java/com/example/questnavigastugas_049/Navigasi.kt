8f72c3833a77fba42d8959b8280a1b6786076603
vararais <varadigmaisnann@gmail.com>
2025-11-09 23:33:05 +0700

    Memanggil nav dan menavigasi awal dari welcome page

--- app/src/main/java/com/example/questnavigastugas_049/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_049/Navigasi.kt
@@ -1,7 +1,32 @@
 package com.example.questnavigastugas_049
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_049.view.FormulirScreen
+import com.example.questnavigastugas_049.view.ListScreen
+import com.example.questnavigastugas_049.view.WelcomeScreen
+
 enum class Screen {
     Welcome,
     List,
     Form
 }
+
+@Composable
+fun TugasAppNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Welcome.name
+    ) {
+        composable(route = Screen.Welcome.name) {
+            WelcomeScreen(
+                onMasukClick = {
+                    navController.navigate(Screen.List.name)
+                }
+            )
+        }
