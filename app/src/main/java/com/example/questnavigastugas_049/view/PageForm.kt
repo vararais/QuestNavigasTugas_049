2ed8acd951e24cdbd77b9aae6d21fa470043835d
vararais <varadigmaisnann@gmail.com>
2025-11-09 22:33:41 +0700

    Menambahkan state dalam Page Form

--- app/src/main/java/com/example/questnavigastugas_049/view/PageForm.kt
+++ app/src/main/java/com/example/questnavigastugas_049/view/PageForm.kt
@@ -1,2 +1,32 @@
 package com.example.questnavigastugas_049.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigastugas_049.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormulirScreen(
+    onBackBtnClick: () -> Unit
+) {
+    var nama by rememberSaveable { mutableStateOf("") }
+    var alamat by rememberSaveable { mutableStateOf("") }
+
+    val jenisKelaminOptions = listOf(
+        stringResource(id = R.string.laki_laki),
+        stringResource(id = R.string.perempuan)
+    )
+    var jenisKelamin by rememberSaveable { mutableStateOf("") }
+
+    val statusOptions = listOf("Janda", "Lajang", "Duda")
+    var statusPerkawinan by rememberSaveable { mutableStateOf("") }
+    var isDropdownExpanded by remember { mutableStateOf(false) }
+
+    var showSuccessDialog by remember { mutableStateOf(false) }
+    var showErrorDialog by remember { mutableStateOf(false) }
